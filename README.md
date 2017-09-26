217ea5c453920a1d8c2f26a58b3c08b79b320551
WataruMaeda <wm19850716@gmail.com>
2017-09-25 23:05:51 -0700

    Added speaker, updated README

--- README.md
+++ README.md
@@ -1,1 +1,79 @@
-# speaker
+# Speaker
+
+## Installation
+
+Add the Speaker to your project manually (drag & drop the file into your project).
+
+## Usage
+
+### ・Basic Usage
+
+1. Import `Speaker` for setup
+
+```Swift
+import Speaker
+```
+2. Pass "text" and "language" to Speaker. That's all!
+
+```Swift
+Speaker.speak(text: "Hello Speaker", language: "en-US")
+```
+
+Here is a list of [supported languages](https://stackoverflow.com/questions/23827145/how-to-get-difference-language-code-for-ios-7-avspeechutterance-text-to-speech)
+
+### ・Interrupt speech
+
+`interrupt` can interrupt the speaking if it in the middle of the speech. The default value is `false`.
+
+```Swift
+Speaker.speak(text: "Hello Speaker", language: "en-US", interrupt: true)
+```
+
+### ・Speech speed
+
+`rate` can adjust the speed of the speech. It is ranged from `0.0` to `1.0`. Default value is `0.0`
+
+```Swift
+Speaker.speak(text: "Hello Speaker", language: "en-US", rate: 1.0)
+```
+### ・Voice pitch
+
+`pitch` can decide the pitch of the voice. Value range is from `0.5` to `2.0`. The default value is `1.0`.
+
+```Swift
+Speaker.speak(text: "Hello Speaker", language: "en-US", pitch: 0.5)
+```
+
+### ・Notice the start and end of the speech
+
+You might like to know when the speech starts and when it ends. In this case, bellow is how to get these callbacks.
+
+```Swift
+Speaker.speak(text: "Hello Speaker", language: "en-US", started: {
+print("Started")
+})
+```
+```Swift
+Speaker.speak(text: "Hello Speaker", language: "en-US", finished: {
+print("Finished")
+})
+```
+You can sets both callbacks
+
+```Swift
+Speaker.speak(text: "Hello Speaker", language: "en-US", started: {
+print("Started")
+}, finished: {
+print("Finished")
+})
+```
+
+## Requirements
+
+iOS 8+
+Swift 3.0+
+Xcode 8.0+
+
+## License
+
+This project is available under the MIT license. See the [LICENSE](https://github.com/WataruMaeda/speaker/blob/master/LICENSE) file for more info.
---
+++ Speaker/Speaker.swift
@@ -0,0 +1,185 @@
+//
+//  Speaker.swift
+//  Speaker
+//
+//  Created by Wataru Maeda on 2017/09/25.
+//  Copyright © 2017 Wataru Maeda. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+// MARK: - Accessor
+
+class Speaker {
+  // Speak based on Country Code
+  class func speech(text: String,
+                    language: String,
+                    interrupt: Bool = false,
+                    rate: Float = 0.0,
+                    pitch: Float = 1.0,
+                    started: @escaping ()->() = {},
+                    finished: @escaping ()->() = {}) {
+    Speech.shared.speak(
+      text: text,
+      language: language,
+      interrupt: interrupt,
+      rate: rate,
+      pitch: pitch,
+      started: started,
+      finished: finished
+    )
+  }
+  
+  // Speak based on Country
+  class func speech(text: String,
+                    country: Country,
+                    interrupt: Bool = false,
+                    rate: Float = 0.0,
+                    pitch: Float = 1.0,
+                    started: @escaping ()->() = {},
+                    finished: @escaping ()->() = {}) {
+    Speech.shared.speak(
+      text: text,
+      language: country.rawValue,
+      interrupt: interrupt,
+      rate: rate,
+      pitch: pitch,
+      started: started,
+      finished: finished)
+  }
+  
+  // Play audio with resorce and type
+  class func play(resource: String,
+                  type: String,
+                  finished: @escaping ()->() = {}) {
+    AudioPlayer.shared.play(
+      resource: resource,
+      type: type,
+      finished: finished
+    )
+  }
+}
+
+// MARK: - Speech
+
+class Speech: NSObject, AVSpeechSynthesizerDelegate {
+  static var shared = Speech()
+  private override init() {}
+  fileprivate lazy var speech = AVSpeechSynthesizer()
+  fileprivate lazy var started: ()->() = {}
+  fileprivate lazy var finished: ()->() = {}
+  
+  func speak(
+    text: String,
+    language: String,
+    interrupt: Bool,
+    rate: Float,
+    pitch: Float,
+    started: @escaping ()->(),
+    finished: @escaping ()->()) {
+    if speech.isSpeaking && interrupt {
+      speech.stopSpeaking(at: .immediate)
+    }
+    self.started = started
+    self.finished = finished
+    speech.delegate = self
+    let utterance = AVSpeechUtterance(string: text)
+    utterance.voice = AVSpeechSynthesisVoice(language: language)
+    utterance.rate = rate
+    utterance.pitchMultiplier = pitch
+    speech.speak(utterance)
+  }
+  
+  // MARK: AVSpeechSynthesizerDelegate
+  
+  func speechSynthesizer(
+    _ synthesizer: AVSpeechSynthesizer,
+    didStart utterance: AVSpeechUtterance) {
+    started()
+  }
+  
+  func speechSynthesizer(
+    _ synthesizer: AVSpeechSynthesizer,
+    didFinish utterance: AVSpeechUtterance) {
+    finished()
+  }
+}
+
+// MARK: - Audio
+
+class AudioPlayer: NSObject, AVAudioPlayerDelegate {
+  static var shared = AudioPlayer()
+  private override init() {}
+  fileprivate lazy var finished: ()->() = {}
+  fileprivate lazy var audioPlayer = AVAudioPlayer()
+  
+  func play(resource: String,
+            type: String,
+            finished: @escaping ()->()) {
+    guard let audioPath = Bundle.main.path(forResource: resource, ofType:type) else {
+      return print("Audio not found")
+    }
+    self.finished = finished
+    play(path: audioPath)
+  }
+  
+  // MARK: AVAudioPlayerDelegate
+  
+  fileprivate func play(path: String) {
+    let audioUrl = URL(fileURLWithPath: path)
+    do {
+      self.audioPlayer = try AVAudioPlayer(contentsOf: audioUrl)
+      self.audioPlayer.delegate = self
+    } catch let error as NSError {
+      print(error)
+    }
+    audioPlayer.play()
+  }
+  
+  func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
+    finished()
+  }
+}
+
+// MARK: - Countries
+
+public enum Country: String {
+  case SaudiArabia = "ar-SA"
+  case China = "zh-CN"
+  case HongKong = "zh-HK"
+  case Taiwan = "zh-TW"
+  case Czech = "cs-CZ"
+  case Denmark = "da-DK"
+  case Belgium = "nl-BE"
+  case Netherlands = "nl-NL"
+  case Australia = "en-AU"
+  case Ireland = "en-IE"
+  case SouthAfrica = "en-ZA"
+  case UK = "en-GB"
+  case US = "en-US"
+  case Finland = "fi-FI"
+  case Canada = "fr-CA"
+  case France = "fr-FR"
+  case Germany = "de-DE"
+  case Greece = "el-GR"
+  case Israel = "he-IL"
+  case India = "hi-IN"
+  case Hungary = "hu-HU"
+  case Indonesia = "id-ID"
+  case Italy = "it-IT"
+  case Japan = "ja-JP"
+  case SouthKorea = "ko-KR"
+  case Norway = "no-NO"
+  case Poland = "pl-PL"
+  case Brazil = "pt-BR"
+  case Portugal = "pt-PT"
+  case Romania = "ro-RO"
+  case Russia = "ru-RU"
+  case Slovakia = "sk-SK"
+  case Mexico = "es-MX"
+  case Spain = "es-ES"
+  case Sweden = "sv-SE"
+  case Thailand = "th-TH"
+  case Turkey = "tr-TR"
+}
